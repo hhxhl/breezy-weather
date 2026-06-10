17d5b31686bddf6a3a1a4d9d69d49568ea73a36a
hhxhl <viphant@gmail.com>
2026-06-10 12:55:01 +0800

    hhxhl

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ configure<ApplicationExtension> {
     namespace = "org.breezyweather"
 
     defaultConfig {
-        applicationId = "org.breezyweather"
+        applicationId = "com.hhxhl.weather"
         versionCode = 60202
         versionName = "6.2.2"
 
